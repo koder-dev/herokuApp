3605f662633daccb1e24da5153e4dd78cd843fb6
Odilov Muhamadkodir <101837517+koder-dev@users.noreply.github.com>
2023-05-21 18:45:09 +0300

    set env

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('hello koder');
+  res.json({ name: 'hello' });
 });
 
 module.exports = router;
